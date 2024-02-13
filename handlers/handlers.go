ffd80bf11c022e0ba94e64167c7fb82c349a02e4
eslam <130592876+ISSHEB@users.noreply.github.com>
2024-02-13 14:24:22 +0300

    first

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -6,6 +6,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 	"net/http"
 	"test/auth"
 	"test/models"
@@ -14,25 +15,42 @@ import (
 var usersCollection *mongo.Collection
 var tokensCollection *mongo.Collection
 
-func LoginHandler(c echo.Context) error {
-	var user models.User
-	if err := c.Bind(&user); err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, "Invalid request")
+func HashToken(password []byte) ([]byte, error) {
+	return bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
+}
+
+func LoginHandler(usersCollection *mongo.Collection, tokensCollection *mongo.Collection) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var user models.User
+		err := json.NewDecoder(r.Body).Decode(&user)
+		if err != nil {
+			http.Error(w, "Invalid request", http.StatusBadRequest)
+			return
+		}
+		var storedUser models.User
+		err = usersCollection.FindOne(context.Background(), bson.M{"_id": user.ID}).Decode(&storedUser)
+		if err != nil {
+			http.Error(w, "User not found", http.StatusNotFound)
+			return
+		}
+		accessToken, refreshToken, err := auth.CreateTokenPair(user.ID.Hex())
+		if err != nil {
+			http.Error(w, "Error creating tokens", http.StatusInternalServerError)
+			return
+		}
+		// Хешируем refresh токен перед сохранением в базу данных
+		hashedRefreshToken, err := HashToken([]byte(refreshToken))
+		if err != nil {
+			http.Error(w, "Error hashing refresh token", http.StatusInternalServerError)
+			return
+		}
+		_, err = tokensCollection.InsertOne(context.Background(), bson.M{"_id": user.ID, "refresh_token": string(hashedRefreshToken)})
+		if err != nil {
+			http.Error(w, "Error saving tokens", http.StatusInternalServerError)
+			return
+		}
+		json.NewEncoder(w).Encode(models.TokenPair{AccessToken: accessToken, RefreshToken: refreshToken})
 	}
-	var storedUser models.User
-	err := usersCollection.FindOne(context.Background(), bson.M{"id": user.ID}).Decode(&storedUser)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusNotFound, "User not found")
-	}
-	accessToken, refreshToken, err := auth.CreateTokenPair(user.ID.Hex())
-	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, "Error creating tokens")
-	}
-	_, err = tokensCollection.InsertOne(context.Background(), bson.M{"_id": user.ID, "refresh_token": refreshToken})
-	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, "Error saving tokens")
-	}
-	return c.JSON(http.StatusOK, models.TokenPair{AccessToken: accessToken, RefreshToken: refreshToken})
 }
 
 func RefreshHandler(c echo.Context) error {
